27f26dfac288f5d7d8ea86237c7d2e28fc4a6933
Nguyen Xuan Cong <nguyenxuancong@outlook.com>
2014-10-23 15:06:44 +0700

    finish chapters

--- client/views/books/book.js
+++ client/views/books/book.js
@@ -109,36 +109,80 @@ Template.chaptersOfBook.helpers({
         var bookId = Router.current().params._id;
         var bookAndPages = bookPages.find({}, {sort: {updatedAt: -1}}).fetch();
         var book = _.findWhere(bookAndPages, {'_id': bookId});
-        var pages = _.where(bookAndPages, {isChapterOfBook : bookId});
+        if (_.isUndefined(book)) return;
+        var pages = _.where(bookAndPages, {isChapterOfBook: bookId});
         var sz = _.size(pages);
-        _.each(pages,function(p){
-            return _.extend(p,{orderRange : _.range(sz,-sz,-1)});
+        _.each(pages, function (p) {
+            return _.extend(p, {orderRange: _.range(sz, -sz, -1)});
         });
 
-        //console.log(pages)
+        if (_.isUndefined(book.chapters)) _.extend(book, {chapters: []});
 
-        if (!book.chapters) {
-            _.extend(book,{chapters : pages})
+        if (!book.chapters || _.size(book.chapters) <= 0) {
+            _.each(pages, function (p) {
+                book.chapters.push({
+                    orderRange: _.range(sz, -sz, -1),
+                    pageId: p._id,
+                    _id: p._id,
+                    titleOfChapter: p.title,
+                    title: p.title,
+                    isPublish: false,
+                    orderNo: 0
+                })
+            })
+        } else {
+            var ids = _.pluck(book.chapters, 'pageId');
+            _.each(book.chapters, function (c) {
+                return _.extend(c, {orderRange: _.range(sz, -sz, -1), '_id': c.pageId});
+            });
+            _.each(pages, function (p) {
+                if (!_.contains(ids, p._id)) {
+                    book.chapters.push({
+                        orderRange: _.range(sz, -sz, -1),
+                        pageId: p._id,
+                        _id: p._id,
+                        titleOfChapter: '',
+                        title: p.title,
+                        isPublish: false,
+                        orderNo: sz
+                    })
+                }
+            })
         }
-        //_.each(book.chapters,function(c){console.log(c.title)})
+        _.sortBy(book.chapters, 'orderNo');
         return book;
     }
 });
 
-Template.chaptersOfBook.events({
-    'click button[id^=btnSave_]' : function(e,t){
-        var title = $(t.find('#titleOfChapter_'+this._id));
+Template.chaptersOfBook.created = function () {
 
+}
+
+Template.chaptersOfBook.rendered = function () {
+    $(document).ready(function () {
+        setTimeout(function () {
+            $('select[id^=orderNo_]').each(function (i, s) {
+                var orderNo = $(s).attr('data-selected');
+                $(s).val(orderNo);
+            })
+        }, 100);
+    })
+}
+
+Template.chaptersOfBook.events({
+    'click button[id^=btnSave_]': function (e, t) {
+        var title = $(t.find('#titleOfChapter_' + this._id));
+        console.log($(title).val())
         var chapter = {
-            bookId : Router.current().params._id,
-            pageId : this._id,
-            orderNo : $(t.find('#orderNo_'+this._id)).val(),
-            titleOfChapter : (_.isEmpty(title.attr('value'))) ? title.attr('placeholder') : title.attr('value'),
-            isPublish : $(t.find('#isPublish_'+this._id)).is(":checked")
+            bookId: Router.current().params._id,
+            pageId: this._id,
+            orderNo: $(t.find('#orderNo_' + this._id)).val(),
+            titleOfChapter: (_.isEmpty($(title).val())) ? $(title).attr('placeholder') : $(title).val(),
+            isPublish: $(t.find('#isPublish_' + this._id)).is(":checked")
         };
 
-        Meteor.call('updateChapter',chapter,function(err,rs){
-            console.log(err,rs);
+        Meteor.call('updateChapter', chapter, function (err, rs) {
+            console.log(err, rs);
         })
     }
 })
